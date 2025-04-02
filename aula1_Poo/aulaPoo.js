156b5e864b2cc3d89891bb8b6b798aaef38bedaf
AndrewsBoddenberg <boddenbergandrews@gmail.com>
2025-04-02 19:35:29 -0300

    Explicação do código

--- aula1_Poo/aulaPoo.js
+++ aula1_Poo/aulaPoo.js
@@ -1,14 +1,18 @@
 const prompt = require("prompt-sync")();
 
+// Classe é o modelo de atributos e funções para a criação de um objeto
 class Cliente {
 
-    nome;
+    nome; // Atributo
     idade;
     cpf;
     agencia;
 }
 
+// Objeto que possui as informações da classe de forma individual
 let cliente = new Cliente ();
+
+
 
 cliente.nome = prompt("Digite o nome do cliente: ");
 cliente.idade = prompt("Digite sua idade ");
